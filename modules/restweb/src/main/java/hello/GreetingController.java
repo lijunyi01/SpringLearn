364db39f64400e88f5d25ea730c8d641a8df5eaf
lijunyi01 <lijunyi01@gmail.com>
2015-05-13 16:00:25 +0800

    comment modify

--- modules/restweb/src/main/java/hello/GreetingController.java
+++ modules/restweb/src/main/java/hello/GreetingController.java
@@ -7,10 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by ljy on 15/5/12.
- * ok
+ * class 上的@RequestMapping("/aaa")注释和方法上的@RequestMapping("/greeting")要叠加
+ * http://localhost:8080/aaa/greeting?name=ljy  才能正常访问；若果class上没有@RequestMapping("/aaa")，则
+ * 访问http://localhost:8080/greeting?name=ljy 即可
  */
 
 @RestController
+@RequestMapping("/aaa")
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
