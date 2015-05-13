39dd0e9c6ac7ec247e457cbc23a0d21bc0d9cc51
lijunyi01 <lijunyi01@gmail.com>
2015-05-13 16:35:53 +0800

    comment modify

--- modules/restweb/src/main/java/hello/InitWeb.java
+++ modules/restweb/src/main/java/hello/InitWeb.java
@@ -5,7 +5,9 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 /*
-这个类的作用与在web.xml中配置负责初始化Spring应用上下文的监听器作用类似，只不过在这里不需要编写额外的XML文件了
+ * 这个类的作用与在web.xml中配置负责初始化Spring应用上下文的监听器作用类似，只不过在这里不需要编写额外的XML文件了
+ * 如果利用Spring boot 将项目作为jar包部署的话，是不需要本类的，jar包方式下入口类为Application; war 包方式下，
+ * 入口类才变为InitWeb 。两种方式对应的pom.xml是不同的
  */
 @ComponentScan
 @EnableAutoConfiguration
