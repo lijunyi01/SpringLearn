115110cc5c9186536917f4dfbf94962fcf6f389d
lijunyi01 <lijunyi01@gmail.com>
2015-05-16 15:55:11 +0800

    comment modify

---
+++ modules/s11-springmvc/src/main/java/allcom2/hello/GreetingController.java
@@ -0,0 +1,19 @@
+package allcom2.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+
+//@RestController   得到的结果是字符串“greeting”,与@Controller的结果是不同的
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
