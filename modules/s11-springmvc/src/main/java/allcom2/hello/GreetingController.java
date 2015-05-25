e27181efa063ef7125c24c27bbe49fec21d45760
lijunyi01 <lijunyi01@gmail.com>
2015-05-25 10:52:19 +0800

    comment modify

--- modules/s11-springmvc/src/main/java/allcom2/hello/GreetingController.java
+++ modules/s11-springmvc/src/main/java/allcom2/hello/GreetingController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
 
 //@RestController   得到的结果是字符串“greeting”,与@Controller的结果是不同的
 @Controller
