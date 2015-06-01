8829eed2adffbe28b990cc6b974b80102f28207c
lijunyi01 <lijunyi01@gmail.com>
2015-06-01 16:52:00 +0800

    comment modify

---
+++ modules/s30-springtransaction/src/main/java/allcom/conf/DatabaseConfig.java
@@ -0,0 +1,13 @@
+package allcom.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * Created by ljy on 15/6/1.
+ * ok
+ */
+@Configuration
+@ImportResource("classpath:/databaseconfig.xml")
+public class DatabaseConfig {
+}
---
+++ modules/s30-springtransaction/src/main/java/allcom/hello/Application.java
@@ -0,0 +1,89 @@
+package allcom.hello;
+
+import javax.sql.DataSource;
+
+import allcom.App;
+import org.junit.Assert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+
+@SpringBootApplication
+@ComponentScan(basePackageClasses = App.class)
+public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    @Bean
+    BookingService bookingService() {
+        return new BookingService();
+    }
+
+    //不用初始化dataSource就能运行是因为：引入了h2database，一种内存数据库，系统自动就初始化数据库并建库testdb了。
+    @Bean
+    JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        log.info("Creating tables");
+        //h2database
+//        jdbcTemplate.execute("drop table BOOKINGS if exists");
+//        jdbcTemplate.execute("create table BOOKINGS("
+//                + "ID serial, FIRST_NAME varchar(5) NOT NULL)");
+        //mysql
+        jdbcTemplate.execute("drop table if exists BOOKINGS");
+        jdbcTemplate.execute("create table BOOKINGS("
+                + "ID serial, FIRST_NAME varchar(5) NOT NULL)");
+        return jdbcTemplate;
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        BookingService bookingService = ctx.getBean(BookingService.class);
+        bookingService.book("Alice", "Bob", "Carol");
+        Assert.assertEquals("First booking should work with no problem", 3,
+                bookingService.findAllBookings().size());
+
+        try {
+            bookingService.book("Chris", "Samuel");
+        }
+        catch (RuntimeException e) {
+            log.info("v--- The following exception is expect because 'Samuel' is too big for the DB ---v");
+            log.error(e.getMessage());
+        }
+
+        for (String person : bookingService.findAllBookings()) {
+            log.info("So far, " + person + " is booked.");
+        }
+        log.info("You shouldn't see Chris or Samuel. Samuel violated DB constraints, and Chris was rolled back in the same TX");
+        Assert.assertEquals("'Samuel' should have triggered a rollback", 3,
+                bookingService.findAllBookings().size());
+
+        try {
+            bookingService.book("Buddy", null);
+        }
+        catch (RuntimeException e) {
+            log.info("v--- The following exception is expect because null is not valid for the DB ---v");
+            log.error(e.getMessage());
+        }
+
+        for (String person : bookingService.findAllBookings()) {
+            log.info("So far, " + person + " is booked.");
+        }
+        log.info("You shouldn't see Buddy or null. null violated DB constraints, and Buddy was rolled back in the same TX");
+        Assert.assertEquals("'null' should have triggered a rollback", 3, bookingService
+                .findAllBookings().size());
+
+    }
+
+}
