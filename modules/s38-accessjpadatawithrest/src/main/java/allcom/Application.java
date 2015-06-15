8445d762b4bdc8fb6d8fccf7e0b7ab00d3d40092
lijunyi01 <lijunyi01@gmail.com>
2015-06-15 16:33:29 +0800

    comment modify

---
+++ modules/s38-accessjpadatawithrest/src/main/java/allcom/Application.java
@@ -0,0 +1,14 @@
+package allcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackageClasses = App.class)
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
