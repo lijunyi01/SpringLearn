79f5b25c22218d060f0c2fb1ad15956b400a32d9
lijunyi01 <lijunyi01@gmail.com>
2019-05-06 06:44:32 +0800

    add s39

--- modules/s39-spring-mybatis/src/main/java/com/allcom/spring_mybatis/dao/AppDao.java
+++ modules/s39-spring-mybatis/src/main/java/com/allcom/spring_mybatis/dao/AppDao.java
@@ -1,10 +1,8 @@
 package com.allcom.spring_mybatis.dao;
 
 import com.allcom.spring_mybatis.entity.Tb1Entity;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.ResultType;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.mapping.ResultSetType;
 import org.apache.ibatis.session.ResultHandler;
 import org.springframework.stereotype.Repository;
 
@@ -19,5 +17,7 @@ public interface AppDao {
 
     @ResultType(value = Tb1Entity.class)
     @Select("select * from tb1")
+    // 分批拉取的关键，否则全部拉取，数据量大时一样会导致oom
+    @Options(fetchSize = -2147483638,resultSetType = ResultSetType.FORWARD_ONLY)
     void getAllTb1Info(ResultHandler<Tb1Entity> handler);
 }
